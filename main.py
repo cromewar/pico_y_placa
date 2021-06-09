acd6e047d7cd667e130bf919e5223b6d85dc6c0e
cromewar <vasiliy@cromewar.com>
2021-06-09 18:18:28 -0500

    main file creation and setup of the virtual env

---
+++ main.py
@@ -0,0 +1,18 @@
+# Using the language that you feel most proficient in, we’d like you to write a "Pico y Placa" predictor. The inputs should be a license plate number (the full number, not the last digit), a date (as a String), and a time, and the program will return whether or not that car can be on the road. You may use any input and output method you prefer. Although it is not required, we’d love to see the following in your code for extra points:
+
+# Good object-oriented code, avoiding repetition and favoring a consistent organization. You should stick to your chosen language’s semantics, and try to be as consistent as possible.
+# Correct usage of version control tools, with a good commit history and incremental software delivery practices.
+# Automated testing with any framework or tool of your choice.
+# We value candidates that love clean, well-structured code, and that can solve problems in a creative way.
+
+# Please, submit the result to a GitHub, GitLab, or Bitbucket repository and send us the URL within a timeframe of 72 hours. Once we receive this information we’ll ask our technical team to review it and we’ll let you know about the next steps of the process. 
+
+# Note: To develop this application you need to consider the past rules of the Pico&Placa. (Hours: 7:00am - 9:30am / 16:00pm - 19:30). Additional research would be needed to complete the exercise. 
+def run():
+    print("test")
+
+if __name__ == "__main__":
+    run()
+
+
+
